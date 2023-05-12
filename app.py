ca264e7cdc91fb3a02c8ea1488aae30f6e0c3af9
Emmanuel Wilford <75589095+Wilfordabird@users.noreply.github.com>
2023-05-12 16:36:11 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import facebook_scraper
+import pandas as pd
+
+def scraping_comments(url):
+    options={"comments": 1000}
+    urls = [url]
+    comment_text = []
+    posts = facebook_scraper.get_posts(options = options, post_urls=urls, credentials=tuple({"username": "wilfordabird", "password": "Awesomeej83!!"}.values()))
+    for post in posts:
+        comments = post["comments_full"]
+        for comment in comments:
+            comment_text.append([comment['commenter_name'], comment["comment_text"]])
+            replies = comment["replies"]
+            for reply in replies:
+                comment_text.append([reply['commenter_name'], reply["comment_text"]])
+    df = pd.DataFrame(comment_text, columns=["commenter_name", "comment_text"])
+    return df
+
+def main():
+    st.image("YIGH-Blue.png")
+    st.title("Facebook Scraper")
+    url = st.text_input("Enter URL")
+    if st.button("Scrape"):
+        comment_text = scraping_comments(url)
+        st.dataframe(comment_text)
+
+
+if __name__ == "__main__":
+    main()
+    #example url: https://www.facebook.com/NBCNews/posts/pfbid02qzHYaSACV21AsHbzUDms5j51yQ18Bc2Xio6gjP1MDsFDVuv4w5yuh6dHMUBTEuEpl
+    #unicef url: https://www.facebook.com/UNICEFSouthAfrica/posts/pfbid088DuwL5BhynA5r7X7DXCqFvCeneR4KtuXii3ARk1bGoK7bh6q7Rqn4PzR1JmJJA6l
+    
